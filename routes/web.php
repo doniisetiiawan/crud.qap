d66bfca9c04acc912aa676b66d97496235a66de8
Doni Setiawan <donisetiawan.050@gmail.com>
2019-12-09 17:12:53 +0700

    Controllers versus routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('contact', [\App\Http\Controllers\PagesController::class, 'contact']);
